098754c1fd202168f1f17decb8f07d8d8484b1b0
Anushree Soman <ANUSHREE.SOMAN@BARUCHMAIL.CUNY.EDU>
2022-11-15 12:02:52 -0500

    Initial Commit

---
+++ SwiftUI_WeatherApp/SwiftUI_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherAppApp.swift
+//  SwiftUI_WeatherApp
+//
+//  Created by Anushree Soman on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
